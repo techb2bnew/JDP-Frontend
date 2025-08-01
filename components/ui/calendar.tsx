52c1fc8c4fe1f528e2b9e6a5cd8c89341b46ce52
saurabh bhatia <s.bhatiaodesk@gmail.com>
2025-08-01 19:25:21 +0530

    ui issue

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -18,7 +18,7 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
-        months: "flex flex-col sm:flex-row gap-2",
+        months: "flex flex-col sm:flex-row gap-2 bg-white",
         month: "flex flex-col gap-4",
         caption: "flex justify-center pt-1 relative items-center w-full",
         caption_label: "text-sm font-medium",
